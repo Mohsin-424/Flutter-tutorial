6e9a99f116e316405e66584af015c0c21db8cecd
Mohsin-424 <mi873345@gmail.com>
2023-08-16 08:48:32 +0500

    Data Type in Dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() {
     const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Pakistan Zindabad'),
+          child: Text('786 Hello there i am Flutter'),
         ),
       ),
     ),
